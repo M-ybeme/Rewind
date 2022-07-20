ff214f0c44cb79a9611c0190ada5b26c4af7e19d
M-ybeme <marlomayberry.90@gmail.com>
2022-07-19 21:20:49 -0400

    link fixes

--- js/site.js
+++ js/site.js
@@ -1,17 +1,36 @@
-function getValue(message){
-    alert("Hello World!");
+//control function
+function getValue() {
+    let message = document.getElementById("message").value;
+    let revMessage = reverse(message);
+    displayMessage(message, revMessage);
 }
 
-function getValue2()
-{
-    let msg = '';
-    msg = document.getElementById("message").value;
 
-    swal.fire(
-    {
-        backdrop: false,
-        title: 'APP NAME',
-        text: msg
+//buisness logic
+function reverse(message) {
+
+    //string an array of characters  with index and values
+    let reversedMesssage = ""
+
+    //decrementing for loop
+    for (let index = message.length-1; index >= 0; index--) {
+
+        reversedMesssage += message[index];
+    }
+    return reversedMesssage;
+}
+
+function displayMessage(message, revMessage){
+
+        if(message.toLowerCase() === revMessage.toLowerCase()){
+        
+            results.innerHTML = "Yes, This is a palindrome.";
+            results.classList.add("success");
+        
+        } else {
+        
+            results.innerHTML = "No, This is not a palindrome.";
+            results.classList.add("fail");
         }
-    )
+        
 }
